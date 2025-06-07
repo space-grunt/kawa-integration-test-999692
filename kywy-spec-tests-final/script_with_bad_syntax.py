b5d5378566035b635dcb7681f1f4aa97383c460b
space-grunt <space-grunt@kawa.ai>
2025-06-08 01:04:58 +0200

    Adding kywy-spec-tests-final/script_with_bad_syntax.py

---
+++ kywy-spec-tests-final/script_with_bad_syntax.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from kywy.client.kawa_decorators import kawa_tool
+
+ABC='text'
+
+@kawa_tool(
+    inputs={'measure1': float},
+    outputs={'decimal': float,
+             'date': dt,
+             'datetime': datetime.datetime,
+             'boolean': bool,
+              ABC: str,
+#            'commented': str
+             'non_commented':str
+            },
+    parameters={'decimal_param': {'type':float, 'default': 1.0},
+                'date_param': {'type':datetime.date, 'default': '2020-01-01'},
+                'datetime_param': {'type':datetime.datetime, 'default': '2000-01-01T01:30:00.000-05:00'},
+                'boolean_param': {'type':bool, 'default': True},
+                'text_param': {'type':str, 'default': 'default'}}
+)
+def tata(df: pd.DataFrame, decimal_param: float, date_param: datetime.date,
+         datetime_param: datetime.datetime, boolean_param: bool, text_param: str):
+    print(df)
+  df['decimal'] = df['measure1'] + decimal_param
+    df['date'] = date_param
+    df['datetime'] = datetime_param
+    df['boolean'] = boolean_param
+    df['text'] = text_param
+    print(df)
+    return df
