140e508c4f51c3318f9879f33a55f35550fa115f
space-grunt <space-grunt@kawa.ai>
2025-06-08 01:04:59 +0200

    Adding kywy-spec-tests-final/test_python_columns_script_measure1.py

---
+++ kywy-spec-tests-final/test_python_columns_script_measure1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from kywy.client.kawa_decorators import kawa_tool
+
+
+@kawa_tool(inputs={'measure1': float}, outputs={'measure3': float})
+def execute_new_decorator(df: pd.DataFrame):
+    print('run: simple_join_script')
+    print('[1]: Data from KAWA')
+    print(df)
+    df['measure3'] = df['measure1'] * 5
+    print('[2]: Data after script operation')
+    print(df)
+    return df
