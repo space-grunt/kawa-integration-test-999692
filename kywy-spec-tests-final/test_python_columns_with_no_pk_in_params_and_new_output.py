43695e9e33b3fcdc4b86f03cf95a7cea51bfd3e6
space-grunt <space-grunt@kawa.ai>
2025-06-08 01:04:59 +0200

    Adding kywy-spec-tests-final/test_python_columns_with_no_pk_in_params_and_new_output.py

---
+++ kywy-spec-tests-final/test_python_columns_with_no_pk_in_params_and_new_output.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from kywy.client.kawa_decorators import kawa_tool
+
+
+@kawa_tool(inputs={'measure1': float, 'measure2': float}, outputs={'measure3': float, 'measure4': float})
+def execute(df: pd.DataFrame):
+    print(df)
+    df['measure3'] = df.apply(lambda row: func(row['measure1'], row['measure2']), axis=1)
+    df['measure4'] = df.apply(lambda row: row['measure1'] * 5, axis=1)
+    return df
+
+
+def func(a: float, b: float):
+    return a + b
